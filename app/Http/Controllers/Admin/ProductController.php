bd5cad9559b23aaeac8a4cb423599020d4fcfaef
Mihai Pantazi <mihai.pantazi193@gmail.com>
2022-08-22 14:23:47 +0300

    Initial commit

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Product;
+use App\Models\ProductImage;
+use App\Models\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\ProductFormRequest;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+
+class ProductController extends Controller
+
+{
+    public function index() {
+        
+         //dd('salut, eu sunt index-ul de produse');
+
+        $products = Product::all();
+        return view('admin.products.index', compact('products'));
+    }
+
+
+    public function create() {
+        
+        //dd('salut, esti pe functia de create products');
+        $categories = Category::all();
+        return view('admin.products.create', compact('categories'));
+    }
+
+
+    public function store(ProductFormRequest $request) 
+    {
+    
+       //dd('esti pe functia store');
+
+       $validatedData = $request->validated();
+        
+       $category = Category::findOrFail($validatedData['category_id']);
+       
+       $product = $category->products()->create([
+            'category_id'=> $validatedData['category_id'],
+            'name'=> $validatedData['name'],
+            'slug'=> Str::slug($validatedData['slug']),
+            'small_description'=> $validatedData['small_description'],
+            'description'=> $validatedData['description'],
+            'original_price'=> $validatedData['original_price'],
+            'selling_price'=> $validatedData['selling_price'],
+            'category_id'=> $validatedData['category_id'],
+            'quantity'=> $validatedData['quantity'],
+            'trending'=> $request->trending == true? '1':'0',
+            'status'=> $request->status == true? '1':'0',
+            'meta_title'=> $validatedData['meta_title'],
+            'meta_keyword'=> $validatedData['meta_keyword'],
+            'meta_description'=> $validatedData['meta_description'],
+        ]);
+
+        if($request->hasFile('image')){
+            $uploadPath = 'uploads/products/';
+
+            $i = 1;
+            foreach($request->file('image') as $imageFile){
+                $extension = $imageFile->getClientOriginalExtension();
+                $filename = time().$i++.'.'.$extension;
+                $imageFile->move($uploadPath,$filename);
+                $finalImagePathName = $uploadPath.$filename;
+
+                $product ->productImages()->create([
+                    'product_id' => $product->id,
+                    'image' => $finalImagePathName,
+                ]);
+            }
+        }
+   
+        return redirect('/admin/products')->with('message', 'Product Added Succesfully');
+    }
+
+
+
+
+
+    public function edit (int $product_id)
+    {
+        //dd ('esti pe functia product edit 😁');    
+        $categories = Category::all();
+        $product = Product::findOrFail($product_id);
+        return view('admin.products.edit', compact('categories', 'product'));
+
+    }
+
+    public function update (ProductFormRequest $request, $product_id)
+    {
+        $validatedData = $request->validated();
+        $product = Category::findOrFail($validatedData['category_id'])
+                        ->products()->where('id',$product_id)->first();
+        if($product)
+        {
+            $product->update([
+                'category_id'=> $validatedData['category_id'],
+                'name'=> $validatedData['name'],
+                'slug'=> Str::slug($validatedData['slug']),
+                'small_description'=> $validatedData['small_description'],
+                'description'=> $validatedData['description'],
+                'original_price'=> $validatedData['original_price'],
+                'selling_price'=> $validatedData['selling_price'],
+                'category_id'=> $validatedData['category_id'],
+                'quantity'=> $validatedData['quantity'],
+                'trending'=> $request->trending == true ? '1':'0',
+                'status'=> $request->status == true ? '1':'0',
+                'meta_title'=> $validatedData['meta_title'],
+                'meta_keyword'=> $validatedData['meta_keyword'],
+                'meta_description'=> $validatedData['meta_description'],
+            ]);
+
+            if($request->hasFile('image')){
+                $uploadPath = 'uploads/products/';
+    
+                $i = 1;
+                foreach($request->file('image') as $imageFile){
+                    $extension = $imageFile->getClientOriginalExtension();
+                    $filename = time().$i++.'.'.$extension;
+                    $imageFile->move($uploadPath,$filename);
+                    $finalImagePathName = $uploadPath.$filename;
+    
+                    $product ->productImages()->create([
+                        'product_id' => $product->id,
+                        'image' => $finalImagePathName,
+                    ]);
+                }
+            }
+       
+            return redirect('/admin/products')->with('message', 'Product Updated Succesfully');
+
+        }
+        else
+        {
+            return redirect('admin/products')->with('message','No such Product is found');
+        }
+    }
+
+
+
+    public function destroyImage(int $product_image_id)
+    {
+        $productImage = ProductImage::findOrFail($product_image_id);
+        if(File::exists($productImage->image)) {
+           File::delete($productImage->image);
+        }
+        $productImage->delete();
+        return redirect()->back()->with('message','Product Image Deleted');
+    }
+
+    public function destroy(int $product_id)
+    {
+        //dd ('ti-am sters produsul! 😃');
+
+        $product = Product::findOrFail($product_id);
+        if($product->productImages) {
+            foreach($product->productImages as $image) {
+                if(File::exists($image->image)) {
+                   File::delete($image->image);
+                }
+            }
+        }
+        $product->delete();
+        return redirect()->back()->with('message','Product Deleted with all its images');
+    }
+}
---
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -0,0 +1,80 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ url('admin/dashboard') }}">
+              <i class="mdi mdi-home menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="mdi mdi-circle-outline menu-icon"></i>
+              <span class="menu-title">Category</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+
+                <li class="nav-item"> <a class="nav-link" href="{{ url('admin/category/create') }}">Create Category</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{ url('admin/category') }}">View Categories</a></li>
+
+              </ul>
+            </div>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#products" aria-expanded="false" aria-controls="products">
+              <i class="mdi mdi-view-headline menu-icon"></i>
+              <span class="menu-title">Products</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="products">
+              <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="{{ url('admin/products/create') }}">Add Product</a></li>
+                      <li class="nav-item"> <a class="nav-link" href="{{ url('admin/products') }}">View Products</a></li>
+              </ul>
+            </div>
+          </li>
+
+              <!-- Produse fara dropdown -->
+                    <!-- <li class="nav-item">
+                                <a class="nav-link" href="{{ url('admin/products') }}">
+                                  <i class="mdi mdi-view-headline menu-icon"></i>
+                                  <span class="menu-title">Products</span>
+                                </a>
+                                  <div class="collapse" id="produse">
+                                    <ul class="nav flex-column sub-menu"> 
+                                          <li class="nav-item"> <a class="nav-link" href="#">Add Product</a></li>
+                                          <li class="nav-item"> <a class="nav-link" href="#">View Products</a></li>
+                                    </ul>
+                                  </div>  
+                    </li> -->
+               <!-- Produse fara dropdown -->
+
+                <!-- <li class="nav-item">
+            <a class="nav-link" href="pages/charts/chartjs.html">
+              <i class="mdi mdi-chart-pie menu-icon"></i>
+              <span class="menu-title">Charts</span>
+            </a>
+          </li> -->
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="pages/tables/basic-table.html">
+              <i class="mdi mdi-grid-large menu-icon"></i>
+              <span class="menu-title">Tables</span>
+            </a>
+          </li> -->
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="pages/icons/mdi.html">
+              <i class="mdi mdi-emoticon menu-icon"></i>
+              <span class="menu-title">Icons</span>
+            </a>
+          </li> -->
+
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="documentation/documentation.html">
+              <i class="mdi mdi-file-document-box-outline menu-icon"></i>
+              <span class="menu-title">Documentation</span>
+            </a>
+          </li> -->
+        </ul>
+      </nav>
