2b32e94aa2cf619b83ed61a1b01344c88160d055
Mihai Pantazi <mihai.pantazi193@gmail.com>
2022-09-04 15:19:35 +0300

    Product view
    Created the and designed he product view,  using livewire.

---
+++ resources/views/livewire/frontend/product/view.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ecommerce Product View Design</title>
+
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="py-3 py-md-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 mt-4">
+                    <div class="bg-white border">
+                        @if($product->productImages)
+                        <img src="{{ asset($product->productImages[0]->image) }}"  class="w-100" alt="Img">
+                        @else
+                        No Image
+                        @endif
+                    </div>
+                </div>
+                <div class="col-md-7 mt-3">
+                    <div class="product-view">
+                        <h4 class="product-name">
+                            {{$product->name}}
+                        </h4>
+                        <hr>
+                        <p class="product-path">
+                            Home / {{ $product->category->name }} / {{ $product->name }}
+                        </p>
+                        <div>
+                            <span class="selling-price">{{ $product->selling_price }} RON</span>
+                            <span class="original-price">{{ $product->original_price }} RON</span>
+                        </div>
+                        <div>
+                            @if ($product->quantity)
+                             <label class="btn-sm py-1 mt-2 text-white bg-success">In Stock</label>
+                             @else
+                              <label class="btn-sm py-1 mt-2 text-white bg-danger">Out of Stock</label>
+                              @endif
+                        </div>
+                        <div class="mt-2">
+                            <div class="input-group">
+                                <span class="btn btn1"><i class="fa fa-minus"></i></span>
+                                <input type="text" value="1" class="input-quantity" />
+                                <span class="btn btn1"><i class="fa fa-plus"></i></span>
+                            </div>
+                        </div>
+                        <div class="mt-2">
+                            <a href="" class="btn btn1"> <i class="fa fa-shopping-cart"></i> Add To Cart</a>
+                            <a href="" class="btn btn1"> <i class="fa fa-heart"></i> Add To Wishlist </a>
+                        </div>
+                        <div class="mt-3">
+                            <h5 class="mb-0">Small Description</h5>
+                            <p>
+                                {{!! $product->small_description }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 mt-3">
+                    <div class="card">
+                        <div class="card-header bg-white">
+                            <h4>Description</h4>
+                        </div>
+                        <div class="card-body">
+                            <p>
+                                {{!! $product->description }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
