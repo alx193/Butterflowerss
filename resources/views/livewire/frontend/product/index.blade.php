b9786507933b639a9b735573cdf45237382e5af1
Mihai Pantazi <mihai.pantazi193@gmail.com>
2022-10-16 13:41:07 +0300

    Added price filter option
    Added the price filter in the products index page

--- resources/views/livewire/frontend/product/index.blade.php
+++ resources/views/livewire/frontend/product/index.blade.php
@@ -1,60 +1,61 @@
-<div class="col-12">
-    <div class="row">
-        
-            
-            
-{{-- 
-                <div class="col-md-3">
-                    <div class="container">
+
+<div class="row">
+    <div class="col">
+                <div class="col-md-9">
+                    <div class="card">
                                 <div class="card-header" ><h4>Pret</h4></div>
                                     <div class="card-body">
-                                        <label class="d-block">
-                                            <input type="radio" name="priceSort" wire:model="priceInput" value="high-to-low" />Descrescator
-                                        </label>
-                                        <label class="d-block">
+                                        <label>
                                             <input type="radio" name="priceSort" wire:model="priceInput" value="low-to-high" />Crescator
+                                        </label>
+                                        <label>
+                                            <input type="radio" name="priceSort" wire:model="priceInput" value="high-to-low" />Descrescat
                                         </label>
                                     </div>
                                 </div>
                     </div>
-                </div> --}}
-                        <div class="row">
-                                @forelse ($products as $productItem)
-                                    <div class="col-md-4">
-                                        <div class="product-card">
-                                            <div class="product-card-img">
-                                                {{-- <label>@if($productItem->quantity > 0)</label>
-                                                <label class="stock bg-success">In Stock</label>
-                                                @else
-                                                <label class="stock bg-danger">Out Of Stock</label>
-                                                @endif --}}
-                                                @if ($productItem->productImages->count() > 0)
-                                                <a href="{{ url('collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
-                                                <img src="{{ asset($productItem->productImages[0]->image) }}" alt="{{ $productItem->name }}">
-                                                </a>
-                                                @endif
-                                            </div>
-                                            <div class="product-card-body">
-                                                <a href="{{ url('collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
-                                                        {{$productItem->name}}
-                                                </a>
-                                                </h5>
-                                                <div>
-                                                    <span class="selling-price">{{$productItem->selling_price}} RON</span>
-                                                    {{-- <span class="original-price">{{$productItem->original_price}} RON</p></span> --}}
+                </div>
+                 <div class="col-md-10">
+                            <div class="row">
+                                    @forelse ($products as $productItem)
+                                        <div class="col-md-4">
+                                            <div class="product-card">
+                                                <div class="product-card-img">
+                                                    {{-- <label>@if($productItem->quantity > 0)</label>
+                                                    <label class="stock bg-success">In Stock</label>
+                                                    @else
+                                                    <label class="stock bg-danger">Out Of Stock</label>
+                                                    @endif --}}
+                                                    @if ($productItem->productImages->count() > 0)
+                                                    <a href="{{ url('collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
+                                                    <img src="{{ asset($productItem->productImages[0]->image) }}" alt="{{ $productItem->name }}">
+                                                    </a>
+                                                    @endif
+                                                </div>
+                                                <div class="product-card-body">
+                                                    <a href="{{ url('collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
+                                                            {{$productItem->name}}
+                                                    </a>
+                                                    {{-- </h5> --}}
+                                                    <div>
+                                                        <span class="selling-price">{{$productItem->selling_price}} RON</span>
+                                                        {{-- <span class="original-price">{{$productItem->original_price}} RON</p></span> --}}
+                                                    </div>
                                                 </div>
                                             </div>
+                                        </div> 
+                                    @empty
+                                        <div class="col-md-12">
+                                            <div class="p-2">
+                                                <h4>No Products Available for {{ $category->name }}</h4>
+                                            </div>
                                         </div>
-                                    </div> 
-                                @empty
-                                    <div class="col-md-12">
-                                        <div class="p-2">
-                                            <h4>No Products Available for {{ $category->name }}</h4>
-                                        </div>
-                                    </div>
-                                @endforelse
-                        </div>
-               
-    </div>         
-    
-</div>
+                                    @endforelse
+                            </div>
+                </div>    
+    </div>   
+</div>      
+
+
+
+
